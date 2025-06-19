5caab7890bda455e0aeb5ebb64718798c5aacc2d
perseosanta <71460746+perseosanta@users.noreply.github.com>
2025-06-19 07:10:17 -0500

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('contact-form').addEventListener('submit', function(e) {
-  e.preventDefault();
-  alert('Formulario enviado. ¡Gracias por contactarnos!');
-  this.reset();
-});
